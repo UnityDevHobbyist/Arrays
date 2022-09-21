54aa95b82c8b66e090bef9325150a3e3daa4408c
DevHobbyist <l3alazskiss15@gmail.com>
2022-09-21 13:35:13 -0300

    Initial commit

---
+++ Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weapons = { "Pistol", "Machine Gun", "Bazooka", "Assault Rifle", "Rocket Launcher", "Grenade" };
+            string[] ammo = { "100", "500", "8", "320", "5", "2" };
+        }
+    }
+}
