e5edf721896b9496cee0acb3f5b9abf3913050f3
DevHobbyist <l3alazskiss15@gmail.com>
2022-10-04 10:04:23 -0300

    Update Program.cs

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,8 +10,13 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            string[] weapons = { "Pistol", "Machine Gun", "Bazooka", "Assault Rifle", "Rocket Launcher", "Grenade" };
-            string[] ammo = { "100", "500", "8", "320", "5", "2" };
+            string[] weapons = {"Pistol", "Machine Gun", "Bazooka", "Assault Rifle", "Rocket Launcher", "Grenade"};
+            string[] ammo = {"100", "500", "8", "320", "5", "2"};
+
+            foreach (String weapon in weapons)
+                Console.WriteLine(weapon);
+
+            Console.ReadKey(false);
         }
     }
 }
